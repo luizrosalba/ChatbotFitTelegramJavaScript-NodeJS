a6ecd3df8271e67786a168b38551cf634426c114
Luiz <luizrosalba@gmail.com>
2020-08-26 09:43:13 -0300

    final_curso

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const TelegramBot = require ('node-telegram-bot-api')
-const bot = require ('./bot.json')
-const dialogflow = require ('./dialogflow')
+const TelegramBot = require ('node-telegram-bot-api');
+const bottext = require ('./bot.json');
+const dialogflow = require ('./dialogflow');
+const youtube = require('./youtube');
+
  
 
-const token = bot.token_uri;
+const token = bottext.token_uri;
 
 // replace the value below with the Telegram token you receive from @BotFather
 //const token = 'YOUR_TELEGRAM_BOT_TOKEN';
@@ -31,15 +33,16 @@ bot.on('message', async function(msg) {
   const chatId = msg.chat.id; /// qual o chat id , a tela utilizada no TG
   console.log(msg.text);
   
-  const dfResponde = dialogflow.sendMessage(chatId.toString(),msg.text);
+  const dfResponse = await dialogflow.sendMessage(chatId.toString(),msg.text);
 
-  if ((dfResponde).intent === 'Treino específico'){
-    
+  let responseText= dfResponse.text; 
+  if ((dfResponse).intent === 'Treino específico'){
+    responseText = await youtube.searchVideoURL(responseText,dfResponse.field.corpo.stringValue);
   }
  
   // send a message to the chat acknowledging receipt of their message
   //bot.sendMessage(chatId, 'Received your message');
-  bot.sendMessage(chatId, dfResponde.text);
+  bot.sendMessage(chatId, responseText);
 
 });
 
--- youtube.js
+++ youtube.js
@@ -16,19 +16,21 @@ youtube.search('Exercicio em casa para bíceps', 2,function(error,result){
 });
 
 function searchVideoURL(message, queryText){
-    return new Promise(resolve,reject ) =>{
-        youtube.search(`Exercício em casa para biceps ${queryText}`, 2 , functon (error, result))
-        {
-            if (!error){
-                console.log(JSON.stringify(result,null,2));
+    return new Promise ((resolve,reject) => {
+        youtube.search(`Exercício em casa para biceps ${queryText}`, 2 , function (error, result) {
+            if (!error) {
+                const videosIds = result.items.map((item)=> item.id.videoId).filter(item=>item); /// retorna so videos com id validos 
+                const youtubeLinks = videosIds.map(videoId => `https://www.youtube.com/watch?v=${videoId}` );
+                resolve(`${message} ${youtubeLinks.join(`, `)}`);
             }
-            else{
-                resolve()
+            else  {
+                reject('Deu erro');
             }
-        }
+        });
     
-    }
+    });
 
 }
 
+module.exports.searchVideoURL = searchVideoURL; 
 /// 
